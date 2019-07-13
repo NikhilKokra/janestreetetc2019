fc014f82c30dedf3a1abfd6ccefa8dd8fc7c2fa2
Nikhil Kokra <nikhil.kokra170@gmail.com>
2019-07-13 16:29:57 -0700

    fixed bonds

--- main.py
+++ main.py
@@ -113,17 +113,23 @@ def adr(conn, valbz, vale):
         return True
     return False
 
+def bonds_helper(conn, price, target, bond_orders_d, target_order):
+    if price not in bond_orders_d:
+        _handle_bond_resp(conn.add_ticker("BOND", target_order, price, target))
+    elif bond_orders_d[price] < target:
+        _handle_bond_resp(conn.add_ticker("BOND", target_order, price, target - bond_orders_d[price]))
 
-def bonds(conn, data=None):
-    global id
-    resp = conn.request({"type": "add", "order_id": id, "symbol": "BOND",
-                         "dir": "BUY", "price": (1000 - random.randint(1, 6)), "size": 10})
-    _handle_bond_resp(resp)
-    id += 1
-    resp = conn.request({"type": "add", "order_id": id, "symbol": "BOND",
-                         "dir": "SELL", "price": (1000 + random.randint(1, 6)), "size": 10})
-    _handle_bond_resp(resp)
-    id += 1
+def bonds(conn):
+    bond_orders = pending_bond_orders.values()
+    bond_orders_d = dict([(a, b) for [a, b] in bond_orders])
+
+    bonds_helper(conn, 997, 43, bond_orders_d, "BUY")
+    bonds_helper(conn, 998, 33, bond_orders_d, "BUY")
+    bonds_helper(conn, 999, 23, bond_orders_d, "BUY")
+    bonds_helper(conn, 1001, 23, bond_orders_d, "SELL")
+    bonds_helper(conn, 1002, 33, bond_orders_d, "SELL")
+    bonds_helper(conn, 1003, 43, bond_orders_d, "SELL")
+
 
 # ~~~~~============== MAIN LOOP ==============~~~~~
 
@@ -151,9 +157,9 @@ def _update_bond_orders(resp):
     if resp['symbol'] == 'BOND':
         if resp['type'] == 'fill':
             if resp['dir'] == 'BUY':
-                pending_bond_orders[resp['order_id']] -= resp['size']
+                pending_bond_orders[resp['order_id']][1] -= resp['size']
             elif resp['dir'] == 'SELL':
-                pending_bond_orders[resp['order_id']] += resp['size']
+                pending_bond_orders[resp['order_id']][1] += resp['size']
         elif resp['type'] == 'out':
             del pending_bond_orders[resp['order_id']]
     print(resp)
@@ -233,9 +239,9 @@ def main():
             print(data)
             if data['type'] == 'book':
                 update_price(conn, data)
-                #bonds(conn, data)
-                etf(conn, data)
-            """
+                bonds(conn)
+                #etf(conn, data)
+
             if last_prices["VALBZ"]["best_bid"] is not None and last_prices["VALE"]["best_bid"] is not None:
                 if adr(conn, last_prices["VALBZ"], last_prices["VALE"]):
                     print("------------------")
@@ -243,7 +249,7 @@ def main():
                     print("DID ADR ARBITRAGE")
                     print("------------------")
                     print("------------------")
-            """
+
 
         except Exception as e:
             print("bonds didnt work")
