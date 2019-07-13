7e5cd2cc89d7e3eb400f81480a4ea42787d9e01d
jaythemishra <jaythemishra@gmail.com>
2019-07-13 11:07:20 -0700

    While loop

--- main.py
+++ main.py
@@ -1,11 +1,95 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 
-# import stuff here
+# ~~~~~==============   HOW TO RUN   ==============~~~~~
+# 1) Configure things in CONFIGURATION section
+# 2) Change permissions: chmod +x bot.py
+# 3) Run in loop: while true; do ./bot.py; sleep 1; done
+
+from __future__ import print_function
+
+import sys
+import socket
+import json
+
+# ~~~~~============== CONFIGURATION  ==============~~~~~
+# replace REPLACEME with your team name!
+team_name = "cablecar"
+# This variable dictates whether or not the bot is connecting to the prod
+# or test exchange. Be careful with this switch!
+test_mode = True
+
+# This setting changes which test exchange is connected to.
+# 0 is prod-like
+# 1 is slower
+# 2 is empty
+test_exchange_index = 2
+prod_exchange_hostname = "production"
+
+port = 25000 + (test_exchange_index if test_mode else 0)
+exchange_hostname = "test-exch-" + \
+    team_name if test_mode else prod_exchange_hostname
+
+# ~~~~~============== NETWORKING CODE ==============~~~~~
+
+
+class Connection(object):
+    def __init__(self, hostname,):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.hostname = hostname
+        self.exchange = self.connect()
+
+    def connect(self):
+        self.s.connect((self.hostname, port))
+        return self.s.makefile('rw', 1)
+
+    def request(self, obj):
+        self.write_to_exchange(obj)
+        return self.read_from_exchange()
+
+    def write_to_exchange(self, obj):
+        json.dump(obj, self.exchange)
+        self.exchange.write("\n")
+
+    def read_from_exchange(self):
+        return json.loads(self.exchange.readline())
+
+
+"""
+
+
+def connect():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print(exchange_hostname)
+    s.connect((exchange_hostname, port))
+    return s.makefile('rw', 1)
+
+
+def write_to_exchange(exchange, obj):
+    json.dump(obj, exchange)
+    exchange.write("\n")
+
+
+def read_from_exchange(exchange):
+    return json.loads(exchange.readline())
+"""
+
+# ~~~~~============== MAIN LOOP ==============~~~~~
 
 
 def main():
-    print("Hello World!")
+    fair_values = {"BOND": 1000, "VALBZ": 0, "VALE": 0,
+                   "GS": 0, "MS": 0, "WFC": 0, "XLF": 0}
+    while True:
+        exchange = connect()
+        write_to_exchange(
+            exchange, {"type": "hello", "team": team_name.upper()})
+        hello_from_exchange = read_from_exchange(exchange)
+        # A common mistake people make is to call write_to_exchange() > 1
+        # time for every read_from_exchange() response.
+        # Since many write messages generate marketdata, this will cause an
+        # exponential explosion in pending messages. Please, don't do that!
+        print("The exchange replied:", hello_from_exchange, file=sys.stderr)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
